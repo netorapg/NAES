a3484a040fabf742aad066b7e603185e2f459bfe
netorag <renatoplatz11@outlook.com>
2025-03-13 20:57:15 -0300

    dando inicio

---
+++ voxa/voxaweb/migrations/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import PaginaInicial
+urlpatterns = [
+   path('', PaginaInicial.as_view(), name='index')
+]
---
+++ voxa/voxaweb/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class PaginaInicial(TemplateView):
+    template_name = 'index.html'
+    
