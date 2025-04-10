d7c23d24db3afdd4a2b8aa19b9f0d0fdd40fbb06
netorag <renatoplatz11@outlook.com>
2025-04-10 17:36:08 -0300

    trabalhando em uma pagina sobre

--- other/sinuca_site/jogo/urls.py
+++ other/sinuca_site/jogo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'jogo'
+
 urlpatterns = [
-    path('', views.sinuca, name='sinuca'),
+    path('', views.sinuca, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
 ]
--- other/sinuca_site/jogo/views.py
+++ other/sinuca_site/jogo/views.py
@@ -1,4 +1,22 @@
+import os
 from django.shortcuts import render
 
 def sinuca(request):
     return render(request, 'jogo/sinuca.html')
+
+def sobre(request):
+    project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    estrutura = []
+    for root, dirs, files in os.walk(project_path):
+        level = root.replace(project_path, '').count(os.sep)
+        indent = ' ' * 4 * level
+        estrutura.append(f"{indent}{os.path.basename(root)}/")
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            estrutura.append(f"{subindent}{f}")
+    
+    context = {
+        'estrutura_projeto': '\n'.join(estrutura),
+        'title': 'Sobre o Projeto'
+    }
+    return render(request, 'jogo/sobre.html')
