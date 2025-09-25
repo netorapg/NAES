88852cc452c9dff6ef55e2683eacf046dc259b01
netorapg <renatoplatz11@outlook.com>
2025-09-25 20:02:38 -0300

    aplicando jquery

--- MSNCLONE/msnclone/usuarios/forms.py
+++ MSNCLONE/msnclone/usuarios/forms.py
@@ -1,7 +1,10 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UsuarioCreationForm(UserCreationForm):
+    
+    telefone = forms.CharField(label='Telefone', max_length=15, required=False)
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'telefone']
